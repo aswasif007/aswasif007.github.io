7d6a66e0f18d7aa4a46e835c43cc38850767cfbb
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-08-26 21:26:21 +0600

    Show About instead of Home

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <home></home>
+    <about></about>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'app',
   components: {
-    Home
+    About
   }
 }
 </script>
