0c11c2043d5706d0abdebd8e385aa1b1ace36fdb
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-08-27 19:46:17 +0600

    Add link-module-alias - And use alias

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import Home from './components/Home.vue'
-import About from './components/About.vue'
+import Home from '@/components/Home.vue'
+import About from '@/components/About.vue'
 
 export default {
   name: 'app',
