742ebeaa24a041a2c0ec9b3c6f7cc07eb83979ec
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-08-28 23:54:24 +0600

    Add animation to portfolio

--- src/sections/portfolio/Portfolio.vue
+++ src/sections/portfolio/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pg-portfolio">
+  <div class="pg-portfolio" :class="{ animate }">
     <section-framework>
       <template v-slot:top>
         <nav-bar label="Portfolio" />
@@ -23,6 +23,9 @@ import SectionFramework from '~src/components/SectionFramework.vue';
 import NavBar from '~src/components/NavBar.vue';
 
 export default {
+  props: {
+    animate: { type: Boolean, default: false },
+  },
   components: {
     SectionFramework,
     NavBar,
@@ -38,6 +41,7 @@ export default {
 
   .title {
     font-size: $topbar-font;
+    opacity: 0;
   }
 
   .thumbnails {
@@ -45,6 +49,7 @@ export default {
     justify-content: center;
 
     p {
+      transform: scale(0);
       width: 240px;
       height: 180px;
       border: 2px solid $light-pink;
@@ -54,6 +59,33 @@ export default {
       flex-direction: column;
       margin: 20px 10px;
       background: rgba($color: #c4c4c4, $alpha: 0.5);
+    }
+  }
+}
+
+.animate {
+  .title {
+    animation: show-up 0.5s ease;
+    animation-delay: 0.5s;
+    animation-fill-mode: forwards;
+  }
+
+  .thumbnails {
+    p {
+      animation: grow-up 0.4s ease;
+      animation-fill-mode: forwards;
+
+      &:nth-child(1) {
+        animation-delay: 1s;
+      }
+
+      &:nth-child(2) {
+        animation-delay: 1.4s;
+      }
+
+      &:nth-child(3) {
+        animation-delay: 1.8s;
+      }
     }
   }
 }
