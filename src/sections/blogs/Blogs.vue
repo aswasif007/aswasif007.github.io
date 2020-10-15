ec109c65f0c230465ed7d979465f814584e10cd0
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-10-16 00:45:25 +0600

    Update blog-section design

--- src/sections/blogs/Blogs.vue
+++ src/sections/blogs/Blogs.vue
@@ -6,27 +6,23 @@
     :msgFragments="msgFragments"
     :cta="cta">
     <template v-slot:showcase>
-      <div class="cards">
-        <card class="card"
-          title="Python in a Node app? Let’s play with Zerorpc!"
-          :tags="['Python', 'Node']"
-          logoName="pyjs"
-          url='https://medium.com/@aswasif007/python-in-a-node-app-lets-play-with-zerorpc-cf47128b0497'
-        />
-        <card class="card"
-          title="A migration from Apache Solr to ElasticSearch"
-          :tags="['Solr', 'ES', 'DataMigration']"
-          logoName="solres"
-          url='https://medium.com/@aswasif007/a-migration-from-apache-solr-to-elasticsearch-bf67801dd6e5'
-        />
-      </div>
+      <fragment>
+        <fragment v-for="{ title, summary } in blogs" :key="title">
+          <div class="title">
+            {{ title }}
+          </div>
+          <div class="summary">
+            {{ summary }}
+          </div>
+        </fragment>
+      </fragment>
     </template>
   </bio-framework>
 </template>
 
 <script>
 import BioFramework from '~src/components/BioFramework.vue';
-import Card from '~src/components/Card.vue';
+import Fragment from '~src/components/Fragment';
 
 export default {
   created() {
@@ -34,7 +30,7 @@ export default {
   },
   components: {
     BioFramework,
-    Card,
+    Fragment,
   },
   props: {
     animate: { type: Boolean, default: false },
@@ -62,6 +58,18 @@ export default {
         'sometimes',
       ];
     },
+    blogs() {
+      return [
+        {
+          title: 'Python in a Node app? Let’s play with Zerorpc!',
+          summary: 'An experiment with python, node and zerorpc on cross-language application development.',
+        },
+        {
+          title: 'A migration from Apache Solr to ElasticSearch',
+          summary: 'Story of a large-scale data migration that I participated in as an enterprise software engineer.',
+        }
+      ];
+    }
   }
 }
 </script>
@@ -72,14 +80,25 @@ export default {
 .pg-blogs {
   position: relative;
 
-  .cards {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  .title {
+    margin: calc(var(--ss-ratio) * 4px) 0;
+    color: $coppar-crayola;
+    font-weight: bold;
+    border-bottom: 1px solid $coppar-crayola;
+    font-size: 125%;
+    padding: calc(var(--ss-ratio) * 4px) 0;
+    position: relative;
+    font-family: 'Sansation';
   }
 
-  .card {
-    margin: 8px;
+  .summary {
+    margin: 4px 0 calc(var(--ss-ratio) * 32px) 0;
+    color: $coppar-crayola;
+    font-family: 'PT Serif';
+
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
 }
 </style>
