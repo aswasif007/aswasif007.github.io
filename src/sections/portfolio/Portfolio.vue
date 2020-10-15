af9329dbae928e58ee855610804726b986b372bc
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-10-16 00:16:24 +0600

    Update portfolio section design

--- src/sections/portfolio/Portfolio.vue
+++ src/sections/portfolio/Portfolio.vue
@@ -6,22 +6,26 @@
     :msgFragments="msgFragments"
     :cta="cta">
     <template v-slot:showcase>
-      <div class="cards">
-        <card class="card"
-          title="pyjs"
-          :tags="['Python', 'Node']"
-          logoName="pyjs"
-          url='https://github.com/aswasif007/pyjs'
-          large
-        />
-      </div>
+      <fragment>
+        <fragment v-for="{ title, description, logo } in projects" :key="title">
+          <div class="title">
+            <div class="logo">
+              <img :src="logo" alt="">
+            </div>
+            {{ title }}
+          </div>
+          <div class="description">
+            {{ description }}
+          </div>
+        </fragment>
+      </fragment>
     </template>
   </bio-framework>
 </template>
 
 <script>
 import BioFramework from '~src/components/BioFramework.vue';
-import Card from '~src/components/Card.vue';
+import Fragment from '~src/components/Fragment';
 
 export default {
   created() {
@@ -29,7 +33,7 @@ export default {
   },
   components: {
     BioFramework,
-    Card,
+    Fragment,
   },
   props: {
     animate: { type: Boolean, default: false },
@@ -57,6 +61,25 @@ export default {
         'pixel-perfact apps',
       ];
     },
+    projects() {
+      return [
+        {
+          title: 'PyJs',
+          description: 'A library to facilitate method invocation from a node module to a python module.',
+          logo: '../../../static/pyjs.svg',
+        },
+        {
+          title: 'Pi Academy Web',
+          description: 'Pi Academy is an academic management system for institutions. This is its web app, which is built as a PWA with Vue. Checkout the demo.',
+          logo: '../../../static/pyjs.svg',
+        },
+        {
+          title: 'Pi Academy API',
+          description: 'This is a RESTful api that encapsulates the data architecture of Pi Academy. Checkout the documentation.',
+          logo: '../../../static/pyjs.svg',
+        }
+      ]
+    }
   }
 }
 </script>
@@ -67,14 +90,38 @@ export default {
 .pg-portfolio {
   position: relative;
 
-  .cards {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  .logo {
+    // height: 100%;
+    position: absolute;
+    top: calc(var(--ss-ratio) * 4px);
+    left: 0;
+    bottom: calc(var(--ss-ratio) * 4px);
+
+    img {
+      height: 100%;
+    }
   }
 
-  .card {
-    margin: 8px;
+  .title {
+    margin: calc(var(--ss-ratio) * 4px) 0;
+    color: $coppar-crayola;
+    font-weight: bold;
+    border-bottom: 1px solid $coppar-crayola;
+    font-size: 125%;
+    padding: calc(var(--ss-ratio) * 4px) 0;
+    padding-left: calc(max(0.7, var(--ss-ratio)) * 42px);
+    position: relative;
+    font-family: 'Sansation';
+  }
+
+  .description {
+    margin: 4px 0 calc(var(--ss-ratio) * 32px) 0;
+    color: $coppar-crayola;
+    font-family: 'PT Serif';
+
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
 }
 </style>
